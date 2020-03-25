189a7cc47eb446c6b2788e71836c383aa153d55e
Lukas Kretschmann <61408019+LukasKretschmann@users.noreply.github.com>
2020-03-25 10:19:44 +0100

    Add files via upload

---
+++ Monte_Carlo.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import random
+import math
+
+i = 10000000
+
+count_inside1 = 0
+for count in range(i):
+    d = math.hypot(random.uniform(0,1),random.uniform(0,1))
+    if d < 1: count_inside1 += 1
+a = 4*count_inside1/count
+
+print("Anzahl von Tropfen im Kreis:",count*5)
+print("Berechneter Wert :",a)
+print("Richtiger Wert für PI:  ",3.14159265359)
+
+
+# In[ ]:
+
+
+
+
